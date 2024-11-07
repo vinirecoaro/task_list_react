119f0b6cfbee6d1eb93c866127a53a37eb483ff0
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-11-07 20:08:53 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Login } from "@mui/icons-material";
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
